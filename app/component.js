ce83d479fa97b0a8addf31da225a38a1880dd83b
Steve Hogg <steve@collecto.io>
2015-10-09 11:56:32 +1300

    Setup babel for React.js use

--- app/component.js
+++ app/component.js
@@ -2,7 +2,7 @@ module.exports = function () {
   var element = document.createElement('h1');
   var d = new Date();
 
-  element.innerHTML = 'Hello world ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+  element.innerHTML = 'Hello world!<br>Time of last refresh: ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
 
   return element;
 };
