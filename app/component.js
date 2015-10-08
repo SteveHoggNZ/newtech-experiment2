bbd0c5fbe85ae961272b722e4e7bbea6f427d2d4
Steve Hogg <steve@collecto.io>
2015-10-09 12:08:52 +1300

    Setup initial JSX

--- app/component.js
+++
@@ -1,8 +0,0 @@
-module.exports = function () {
-  var element = document.createElement('h1');
-  var d = new Date();
-
-  element.innerHTML = 'Hello world!<br>Time of last refresh: ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
-
-  return element;
-};
