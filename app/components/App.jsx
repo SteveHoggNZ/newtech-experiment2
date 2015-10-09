1fb29822ec824d14afd25b95773435a5f00a82f3
Steve Hogg <steve@collecto.io>
2015-10-09 19:19:29 +1300

    Support note editing

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -27,14 +27,16 @@ export default class App extends React.Component {
       ]
     };
 
+    this.findNote = this.findNote.bind(this);
     this.addNote = this.addNote.bind(this);
+    this.editNote = this.editNote.bind(this);
   }
   render() {
   	const notes = this.state.notes;
     return (
       <div>
       	<button className="add-note" onClick={this.addNote}>+</button>
-        <Notes items={notes}/>
+        <Notes items={notes} onEdit={this.editNote} />
       </div>
     );
   }
@@ -45,5 +47,27 @@ export default class App extends React.Component {
         task: 'New task'
       }])
     });
+  }
+  editNote(id, task) {
+    let notes = this.state.notes;
+    const noteIndex = this.findNote(id);
+
+    if(noteIndex < 0) {
+      return;
+    }
+
+    notes[noteIndex].task = task;
+
+    this.setState({notes});
+  }
+  findNote(id) {
+    const notes = this.state.notes;
+    const noteIndex = notes.findIndex((note) => note.id === id);
+
+    if(noteIndex < 0) {
+      console.warn('Failed to find note', notes, id);
+    }
+
+    return noteIndex;
   }
 }
